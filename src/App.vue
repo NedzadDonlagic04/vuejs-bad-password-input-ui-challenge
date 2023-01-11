9c0c3b6224ec0557801c1aa47863c516fe075bd7
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-01-11 19:11:26 +0100

    Added way to store and generate random characters

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 
-const passwordParagraph = ref(null);
+const myCanvas = ref(null);
+const password = ref('');
+
+// Uppercase letters in the english
+// alphabet go from 65 - 90 in the ASCII code
+const generateRandomChar = () => {
+    const charCode = Math.floor(Math.random() * 25 + 65);
+
+    return String.fromCharCode(charCode);
+}
 
 onMounted(() => {
-    console.log(passwordParagraph.value);
+    console.log(generateRandomChar());
 });
 
 </script>
 
 <template>
     <p>Input your password</p>
-    <p ref="passwordParagraph">Password goes here</p>
-    <canvas id="myCanvas"></canvas>
+    <p>{{password}}</p>
+    <canvas id="myCanvas" ref="myCanvas"></canvas>
 </template>
 
 <style scoped>
