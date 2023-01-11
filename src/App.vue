d08bf2104a535270bf3c27343b04d86a0d5416b9
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-01-11 18:57:50 +0100

    Added template, style and script tags
    Template tag - added paragraphs to represent the
    needed text and password input, as well as canvas
    element
    Style tag - scoped it, as well as gave styles to
    the p elements to have some space between them,
    position canvas across entire screen
    Script tag - going with the setup API, added
    some code for testing

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const passwordParagraph = ref(null);
+
+onMounted(() => {
+    console.log(passwordParagraph.value);
+});
+
+</script>
+
 <template>
-    <p>Hello World!</p>
+    <p>Input your password</p>
+    <p ref="passwordParagraph">Password goes here</p>
+    <canvas id="myCanvas"></canvas>
 </template>
+
+<style scoped>
+    p {
+        padding: .5rem;
+    }
+
+    #myCanvas {
+        position: fixed;
+        inset: 0;
+        height: 100%;
+        width: 100%;
+    }
+</style>
